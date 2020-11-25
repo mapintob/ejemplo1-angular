718cf366a728c6ab9436c4fd200fe78d20725c06
Miguel <mpinto@academicos.uta.cl>
2020-11-25 20:30:34 -0300

    primer commmit

---
+++ src/app/componentes/body/body.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-body',
+  templateUrl: './body.component.html',
+  styleUrls: ['./body.component.css']
+})
+export class BodyComponent {
+  mostrar = true;
+
+  
+
+
+  personajes:  string [] = ['mario','luigi','koopa'];
+
+}
